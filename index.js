bd7e19e0564b5a61df3c1cd18e0d7a98eb64076c
michaelKurowski <Byamarro@gmail.com>
2019-03-05 22:05:27 +0100

    socketIO connection draft

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const app = require('express')()
 const http = require('http').Server(app)
+const io = require('socket.io')(http)
 
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
 	res.sendFile(`${__dirname}/frontend/index.html`)
 })
 
-http.listen(3000, function(){
+io.on('connection', socket => {
+	console.log('a user connected')
+	socket.on('disconnect', () => {  
+		console.log('user disconnected')
+	})
+})
+
+http.listen(3000, () => {
 	console.log('listening on *:3000')
 })
