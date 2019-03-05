8b249c18f575615b3f5e97948f996fe66eebf4b7
michaelKurowski <Byamarro@gmail.com>
2019-03-05 21:54:05 +0100

    fixed linting errors

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const app = require('express')();
-const http = require('http').Server(app);
+const app = require('express')()
+const http = require('http').Server(app)
 
 app.get('/', function(req, res){
-    res.sendFile(`${__dirname}/frontend/index.html`);
-});
+	res.sendFile(`${__dirname}/frontend/index.html`)
+})
 
 http.listen(3000, function(){
-    console.log('listening on *:3000');
-});
+	console.log('listening on *:3000')
+})
